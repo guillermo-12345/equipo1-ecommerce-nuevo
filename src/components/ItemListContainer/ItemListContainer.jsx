6ecf0693b74a281c108546168ec699de1b5e7da3
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-13 15:51:46 -0300

    vercel7

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,4 @@
-/* import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ItemList from "../ItemList/ItemList";
 import { useParams } from 'react-router-dom';
@@ -47,45 +47,3 @@ const ItemListContainer = ({ greeting }) => {
 export default ItemListContainer;
 
 
- */
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
-const ItemListContainer = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        // Utiliza la URL base desde la variable de entorno
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/products`);
-
-        console.log("Respuesta de la API:", response.data);
-
-        // Validación: verifica que sea un arreglo
-        const data = Array.isArray(response.data) ? response.data : [];
-        setProducts(data.filter((product) => product.stock > 0)); // Filtra productos con stock
-      } catch (error) {
-        console.error("Error al obtener los productos:", error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  return (
-    <div>
-      <h1>Lista de Productos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.title} - Stock: {product.stock}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default ItemListContainer;
