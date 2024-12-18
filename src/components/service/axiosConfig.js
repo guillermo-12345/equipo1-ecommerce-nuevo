9954508932815efb6f00ba007fcccda36b26915d
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-18 12:30:09 -0300

    vercel55b

--- src/components/service/axiosConfig.js
+++ src/components/service/axiosConfig.js
@@ -7,60 +7,7 @@ const axiosInstance = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
-  },
-  timeout: 10000 // 10 second timeout
+  }
 });
 
-// Request interceptor
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // Ensure URL starts with /api
-    if (!config.url.startsWith('/api') && !config.url.startsWith('http')) {
-      config.url = `/api${config.url}`;
-    }
-    
-    console.log('[API Request]:', {
-      method: config.method?.toUpperCase(),
-      url: `${config.baseURL}${config.url}`,
-      headers: config.headers
-    });
-    
-    return config;
-  },
-  (error) => {
-    console.error('[API Request Error]:', error);
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor
-axiosInstance.interceptors.response.use(
-  (response) => {
-    console.log('[API Response]:', {
-      status: response.status,
-      url: response.config.url,
-      data: response.data
-    });
-    return response;
-  },
-  (error) => {
-    if (error.response) {
-      console.error('[API Response Error]:', {
-        status: error.response.status,
-        url: error.config?.url,
-        data: error.response.data
-      });
-    } else if (error.request) {
-      console.error('[API Network Error]:', {
-        url: error.config?.url,
-        message: error.message
-      });
-    } else {
-      console.error('[API Error]:', error.message);
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default axiosInstance;
-
