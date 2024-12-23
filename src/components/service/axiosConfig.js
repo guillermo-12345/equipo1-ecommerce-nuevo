013ac26d884d4c7d8ce9799eadc455f3c62e7b34
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-23 13:43:00 -0300

    vercel611111b

--- src/components/service/axiosConfig.js
+++ src/components/service/axiosConfig.js
@@ -6,16 +6,6 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 15000, // 15 segundos de timeout
 });
-
-// Interceptor para registrar errores
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error('[API Error]:', error.response?.data || error.message);
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
