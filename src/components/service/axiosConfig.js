52f68bdbd81e50d1797c6f0f1210ffccd721c74b
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-23 15:51:35 -0300

    vercel6223456b

--- src/components/service/axiosConfig.js
+++ src/components/service/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://nombre-backend-vercel.vercel.app';
+const baseURL = process.env.REACT_APP_API_URL || 'https://nombre-backend-vercel.vercel.app';
 
 const axiosInstance = axios.create({
   baseURL,
